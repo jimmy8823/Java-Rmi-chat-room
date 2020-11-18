1fd243eb71be6aaa28d68d17416248748ffc22f6
User <User@DESKTOP-E4KN23N>
2020-11-18 15:57:25 +0800

    test

---
+++ Client.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class client 
+{
+	public static void main(String args[])
+	{
+		JFrame login = new JFrame("Login");
+	}
+}
